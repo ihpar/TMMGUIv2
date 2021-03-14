4856e08f5050b6a9edb7e5997f23745c42f70895
ihpar <ismail@cs.deu.edu.tr>
2021-03-14 14:11:56 +0300

    Initial commit

---
+++ services.php
@@ -0,0 +1,32 @@
+<?php
+    error_reporting(E_ERROR | E_PARSE);
+    try {
+        $str_json = file_get_contents("php://input");
+        $client_params = json_decode($str_json, true);
+
+        $makam = $client_params["makam"];
+        $notes = $client_params["notes"];
+
+        $venv_dir = "D:/Deve/Pyt/TmmGui/venv/Scripts/";
+        $worker_dir = "D:/Deve/Pyt/TmmGui/src/";
+        $worker = "phpworker.py ";
+        $worker_args = json_encode($makam) . " " . json_encode($notes);
+        # $cmd = $venv_dir . "python " . $worker_dir . $worker . $worker_args . " > /dev/null 2>/dev/null &";
+        $cmd = $venv_dir . "python " . $worker_dir . $worker . $worker_args;
+
+        pclose(popen("start /b " . $cmd, "r"));
+
+        # shell_exec($cmd);
+
+        $res = [
+            "res" => "OK",
+            "makam" => $makam,
+            "notes" => $notes
+        ];
+
+        echo(json_encode($res));
+
+    } catch (Exception $e) {
+        echo "error-exception-" . $e->getMessage();
+    }
+?>
